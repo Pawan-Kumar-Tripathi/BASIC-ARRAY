26d82cbec5d1e4afc88cc733a8416202dc31a124
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 17:13:30 +0530

    Add files via upload

---
+++ Basicarr.java
@@ -0,0 +1,24 @@
+import java .util.*;
+class Basicarr
+{
+    public static void main()
+    { 
+        Scanner sc = new Scanner(System.in);
+        int i ;
+       // int size = sc.nextInt();
+       // int ar[] = new  int [size];
+       
+       int ar[] = new int [5];
+        for( i=0; i <5;i++)
+        {
+            System.out.println("bhai elemnet dalo");
+            ar[i]=sc.nextInt();
+        
+    }
+    for(int j=0;j<i;j++)
+    {
+    System.out.println("the array is "+ ar[j]);
+}
+}
+}
+ 
